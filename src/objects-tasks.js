9f80f9f5bf055ccc1b04cbd8f5a4c2f9ffa11d34
ZhannaIvashova <zhannaklepikova90@gmail.com>
2025-03-02 10:38:34 +0300

    feat: add tasks shallowCopy and mergeObjects

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -17,8 +17,9 @@
  *    shallowCopy({a: 2, b: { a: [1, 2, 3]}}) => {a: 2, b: { a: [1, 2, 3]}}
  *    shallowCopy({}) => {}
  */
-function shallowCopy(/* obj */) {
-  throw new Error('Not implemented');
+function shallowCopy(obj) {
+  const newObj = { ...obj };
+  return newObj;
 }
 
 /**
@@ -32,8 +33,18 @@ function shallowCopy(/* obj */) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const newObj = {};
+  objects.forEach((obj) => {
+    Object.keys(obj).forEach((key) => {
+      if (newObj[key]) {
+        newObj[key] += obj[key];
+      } else {
+        newObj[key] = obj[key];
+      }
+    });
+  });
+  return newObj;
 }
 
 /**
