d60585198f758ff6d810f12127469bacc86d0b1d
ZhannaIvashova <zhannaklepikova90@gmail.com>
2025-03-05 03:17:24 +0300

    feat: add task makeWord

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -117,8 +117,8 @@ function isEmptyObject(obj) {
  *    immutableObj.newProp = 'new';
  *    console.log(immutableObj) => {a: 1, b: 2}
  */
-function makeImmutable(/* obj */) {
-  throw new Error('Not implemented');
+function makeImmutable(obj) {
+  return Object.freeze({ ...obj });
 }
 
 /**
@@ -131,8 +131,16 @@ function makeImmutable(/* obj */) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const resultArr = [];
+  const entries = Object.entries(lettersObject);
+  for (let i = 0; i < entries.length; i += 1) {
+    const [letter, positions] = entries[i];
+    for (let j = 0; j < positions.length; j += 1) {
+      resultArr[positions[j]] = letter;
+    }
+  }
+  return resultArr.join('');
 }
 
 /**
